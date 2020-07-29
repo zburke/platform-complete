78de96f394a0e70a7dd56eda53498a91334b7478
Maxim Didenko <maxim_didenko@epam.com>
2020-07-29 15:04:21 +0300

    vega specific ui platform

--- stripes.config.js
+++ stripes.config.js
@@ -3,73 +3,34 @@
 // const { merge } = require('lodash');
 
 module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
-  config: {
-    logCategories: 'core,path,action,xhr',
-    logPrefix: '--',
-    showPerms: false
-  },
-
-  modules: {
-    '@folio/acquisition-units': {},
-    '@folio/agreements' : {},
-    '@folio/calendar' : {},
-    '@folio/checkin' : {},
-    '@folio/checkout' : {},
-    '@folio/circulation' : {},
-    '@folio/circulation-log' : {},
-    '@folio/courses' : {},
-    '@folio/data-import' : {},
-    '@folio/data-export' : {},
-    '@folio/developer' : {},
-    '@folio/eholdings' : {},
-    '@folio/erm-comparisons': {},
-    '@folio/erm-usage' : {},
-    '@folio/inventory' : {},
-    '@folio/invoice' : {},
-    '@folio/finance' : {},
-    '@folio/licenses' : {},
-    '@folio/local-kb-admin': {},
-    '@folio/marccat' : {},
-    '@folio/myprofile' : {},
-    '@folio/notes' : {},
-    '@folio/oai-pmh' : {},
-    '@folio/orders' : {},
-    '@folio/organizations' : {},
-    '@folio/plugin-create-inventory-records' : {},
-    '@folio/plugin-create-item' : {},
-    '@folio/plugin-find-agreement' : {},
-    '@folio/plugin-find-erm-usage-data-provider' : {},
-    '@folio/plugin-find-import-profile' : {},
-    '@folio/plugin-find-instance' : {},
-    '@folio/plugin-find-contact' : {},
-    '@folio/plugin-find-eresource': {},
-    '@folio/plugin-find-interface' : {},
-    '@folio/plugin-find-license' : {},
-    '@folio/plugin-find-organization' : {},
-    '@folio/plugin-find-package-title': {},
-    '@folio/plugin-find-po-line': {},
-    '@folio/plugin-find-user' : {},
-    '@folio/quick-marc' : {},
-    '@folio/receiving' : {},
-    '@folio/requests' : {},
-    '@folio/search' : {},
-    '@folio/servicepoints' : {},
-    '@folio/stripes-erm-components' : {},
-    '@folio/tags' : {},
-    '@folio/tenant-settings' : {},
-    '@folio/users' : {}
-  },
-
-  branding: {
-    logo: {
-      src: './tenant-assets/opentown-libraries-logo.png',
-      alt: 'Opentown Libraries',
+    okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+    config: {
+        logCategories: 'core,path,action,xhr',
+        logPrefix: '--',
+        showPerms: false
     },
-    favicon: {
-      src: './tenant-assets/folio-favicon.png',
+
+    modules: {
+        '@folio/calendar' : {},
+        '@folio/checkin' : {},
+        '@folio/checkout' : {},
+        '@folio/circulation' : {},
+        '@folio/inventory' : {},
+        '@folio/plugin-find-user' : {},
+        '@folio/requests' : {},
+        '@folio/servicepoints' : {},
+        '@folio/users' : {}
     },
-  },
+
+    branding: {
+        logo: {
+            src: './tenant-assets/opentown-libraries-logo.png',
+            alt: 'Opentown Libraries',
+        },
+        favicon: {
+            src: './tenant-assets/folio-favicon.png',
+        },
+    },
 };
 
 // module.exports = merge({}, platformCore, platformERM, platformComplete);
