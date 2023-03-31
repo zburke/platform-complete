56a33c13100b3c2790d622766b053420c75d9988
Zak Burke <zburke@ebsco.com>
2023-03-31 16:57:42 -0400

    committing ui-licenses#ERM-2893

--- stripes.config.js
+++ stripes.config.js
@@ -3,7 +3,7 @@
 // const { merge } = require('lodash');
 
 module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  okapi: { 'url':'https://folio-snapshot-okapi.dev.folio.org', 'tenant':'diku' },
   config: {
     logCategories: 'core,path,action,xhr',
     logPrefix: '--',
